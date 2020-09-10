bebde8e6320205f1a4246525486c2a54809b4160
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2020-09-10 20:03:32 -0300

    feature(app): Configuração de arquivos estaticos

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
     //const mongosse = require('mongoose');
     const app = express();
     const admin = require('./routes/admin');
+    const path = require('path');
 //Configurações 
     //Body Parser 
         app.use(bodyParser.urlencoded({extended: true}));
@@ -12,8 +13,14 @@
     //Handlebars
         app.engine('handlebars', handlebars({defaultLayout: 'main'}))
         app.set('view engine','handlebars')
+    
+    
+    
 
+    //Public
+        app.use(express.static(path.join(__dirname,"public")))
 //Rotas 
+
     app.use('/admin', admin)
 
 //Notas
