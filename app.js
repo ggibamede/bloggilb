79bca1eac16b5a53f409e2c9a13fe69338eab3b2
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2020-09-14 15:24:45 -0300

    feature(server): Configuração de sessões

--- app.js
+++ app.js
@@ -6,7 +6,24 @@
     const app = express();
     const admin = require('./routes/admin');
     const path = require('path');
+    const session = require('express-session')
+    const flash = require('connect-flash')
 //Configurações 
+    //Sessão
+        app.use(session({
+            secret: "cursonode",
+            resave: true,
+            saveUninitialized: true
+        }))
+        app.use(flash())
+    
+    //MidleWare
+        app.use((req , res , next)=>{
+            res.locals.success_msg = req.flash("success_msg")
+            res.locals.error_msg = req.flash("error_msg")
+            next()
+        })
+
     //Body Parser 
         app.use(bodyParser.urlencoded({extended: true}));
         app.use(bodyParser.json())
