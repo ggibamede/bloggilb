928c371b2d1a8337595f5a9d738a31152e1db9fe
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2020-09-21 23:20:58 -0300

    feature(server + pag): Salvando postagens no banco de dados

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+//Models de categoria (Categoria.js)
 require('../models/Categoria')
 const Categoria = mongoose.model('categorias')
+//Models de postagens (Postagem.js)
+require('../models/Postagem')
+const Postagem = mongoose.model('postagens')
 
 router.get('/',(req , res)=>{
     res.render("admin/index")
@@ -110,4 +114,33 @@ router.get('/postagens/add',(req , res)=>{
     })
 })
 
+router.post('/postagens/nova',(req, res)=>{
+
+    var erros = []
+
+    if(req.body.categoria == "0"){
+        erros.push({texto: "Categoria invalida, registre uma categoria"})
+    }
+    if(erros.length > 0){
+        res.render('admin/addpostagem',{erros: erros});
+    }else{
+        const novaPostagem = {
+            titulo: req.body.titulo,
+            slug: req.body.slug,
+            descricao: req.body.descricao,
+            conteudo: req.body.conteudo,
+            categoria: req.body.categoria
+        }
+
+        new Postagem(novaPostagem).save().then(()=>{
+            req.flash('success_msg','A postagem foi cadastrada')
+            res.redirect('/admin/postagens')
+        }).catch((err)=>{
+            req.flash('error_msg','Houve um erro na criação da postagem')
+            res.redirect('/admin/postagens')
+        })
+    }
+
+
+})
 module.exports = router
