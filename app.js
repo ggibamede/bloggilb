dff2b5efa2d8221b1f110ef4f2fe5dabaf1f1016
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2020-09-23 10:18:38 -0300

    feature(pag): Home page

--- app.js
+++ app.js
@@ -8,6 +8,8 @@
     const path = require('path');
     const session = require('express-session')
     const flash = require('connect-flash')
+    require('./models/Postagem');
+    const Postagem = mongose.model('postagens')
 //Configurações 
     //Sessão
         app.use(session({
@@ -45,6 +47,18 @@
         
         
 //Rotas 
+    app.get('/', function(req, res){
+        Postagem.find().populate('categoria').sort({data: 'desc'}).then(function(postagens){
+            res.render('index', {postagens: postagens.map((postagem) => postagem.toJSON())});
+        }).catch((err) => {
+            req.flash('error_msg', 'Houve um erro ao listar postagens');
+            res.redirect('/404');
+        });
+    });
+
+    app.get('/404',(req, res)=>{
+        res.send('Erro 404!')
+    })
 
     app.use('/admin', admin)
 
